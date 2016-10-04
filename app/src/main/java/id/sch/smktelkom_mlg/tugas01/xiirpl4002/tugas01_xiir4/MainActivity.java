a1411ca35876b9f04beab67e5e058ba10f6afb65
Aisyah <aisyah214.ank@gmail.com>
2016-10-05 06:56:26 +0700

    Commit kedua koding

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4002/tugas01_xiir4/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4002/tugas01_xiir4/MainActivity.java
@@ -2,12 +2,111 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl4002.tugas01_xiir4;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    RadioButton rbD, rbB;
+    CheckBox cbPulang, cbPergi, cbPP;
+    EditText etNama, etAlamat, etTelp, etNama2, etID, etNamaKA, etAsal, etTujuan, etTgl, etTime;
+    TextView tvHasil, tvHasil2, tvHasil3, tvHasil4, tvHasil5, tvHasil6, tvHasil7, tvHasil8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rbD = (RadioButton) findViewById(R.id.radiButtonDewasa);
+        rbB = (RadioButton) findViewById(R.id.radiButtonBayi);
+
+        cbPergi = (CheckBox) findViewById(R.id.checkBoxPergi);
+        cbPulang = (CheckBox) findViewById(R.id.checkBoxPulang);
+        cbPP = (CheckBox) findViewById(R.id.checkBoxPP);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        etTelp = (EditText) findViewById(R.id.editTextTelp);
+        etNama2 = (EditText) findViewById(R.id.editTextNama2);
+        etID = (EditText) findViewById(R.id.editTextID);
+        etNamaKA = (EditText) findViewById(R.id.editTextNamaKA);
+        etAsal = (EditText) findViewById(R.id.editTextAsal);
+        etTujuan = (EditText) findViewById(R.id.editTextTujuan);
+        etTgl = (EditText) findViewById(R.id.editTextTgl);
+        etTime = (EditText) findViewById(R.id.editTextTime);
+
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil2 = (TextView) findViewById(R.id.textViewHasil2);
+        tvHasil3 = (TextView) findViewById(R.id.textViewHasil3);
+        tvHasil4 = (TextView) findViewById(R.id.textViewHasil4);
+        tvHasil5 = (TextView) findViewById(R.id.textViewHasil5);
+        tvHasil6 = (TextView) findViewById(R.id.textViewHasil6);
+        tvHasil7 = (TextView) findViewById(R.id.textViewHasil7);
+        tvHasil8 = (TextView) findViewById(R.id.textViewHasil8);
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doClick();
+            }
+        });
     }
+
+    private void doClick() {
+        String hasil = "PESANAN TIKET : ";
+        int startlen = hasil.length();
+        if (cbPergi.isChecked()) {
+            hasil = cbPergi.getText().toString();
+        } else if (cbPulang.isChecked()) {
+            hasil = cbPulang.getText().toString();
+        } else if (cbPP.isChecked()) {
+            hasil = cbPP.getText().toString();
+        }
+
+        if (hasil.length() == startlen) hasil += "Tidak ada pada Pilihan";
+        String tujuan = etTujuan.getText().toString();
+        tvHasil.setText(hasil + " TUJUAN " + tujuan);
+
+        doProses();
+    }
+
+    private void doProses() {
+        if (isValid()) {
+            String namaKA = etNamaKA.getText().toString();
+            tvHasil2.setText(namaKA);
+
+            String type = null;
+            if (rbD.isChecked()) {
+                type = rbD.getText().toString();
+            } else if (rbB.isChecked()) {
+                type = rbB.getText().toString();
+            }
+
+            if (type == null) {
+                tvHasil8.setText("Belum memilih type");
+            } else {
+                tvHasil8.setText("Type : " + type);
+            }
+
+            String nama = etNama2.getText().toString();
+            tvHasil3.setText("NAMA : " + nama);
+            String id = etID.getText().toString();
+            tvHasil4.setText("NO IDENTITAS : " + id);
+            String asal = etAsal.getText().toString();
+            String time = etTime.getText().toString();
+            tvHasil5.setText(asal + " : " + time);
+            String tgl = etTgl.getText().toString();
+            tvHasil6.setText("TGL BERANGKAT : " + tgl);
+            tvHasil7.setText("Rp. 15.000,-");
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        return valid;
+    }
+
 }
